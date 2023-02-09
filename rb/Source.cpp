a7087682452946812f01e547d6f1afafd86ffb61
sultanovN <104188736+sultanovN@users.noreply.github.com>
2023-02-09 10:22:06 +0600

    Enemy drawing fixed

--- rb/Source.cpp
+++ rb/Source.cpp
@@ -1,6 +1,8 @@
 #include "raylib.h"
 #include <vector>
 
+bool game_over = false;
+
 typedef struct Timer {
 	double startTime;   // Start time (seconds)
 	double lifeTime;    // Lifetime (seconds)
@@ -22,7 +24,9 @@ double GetElapsed(Timer timer)
 	return GetTime() - timer.startTime;
 }
 
+
 class Player;
+
 
 class Projectile
 {
@@ -39,6 +43,8 @@ public:
 
 	float getY()const { return m_y; }
 
+	Vector2 getVec() const { return { m_x, m_y }; }
+
 	Projectile draw()
 	{
 		DrawCircle(m_x, m_y, m_radius, WHITE);
@@ -50,7 +56,21 @@ public:
 		m_y -= m_speed * GetFrameTime();
 		return *this;
 	}
+
+	/*void PlayerAttacked(Player &player)
+	{
+		if (CheckCollisionPointTriangle(getVec(), player.getPoint1(), player.getPoint2(), player.getPoint3()))
+		{
+			game_over = true;
+		}
+	}
+
+	void EnemyAttacked()
+	{
+
+	}*/
 };
+
 
 class Player
 {
@@ -67,13 +87,22 @@ public:
 
 	}
 
+	~Player()
+	{
+
+	}
+
+	Vector2 getPoint1()const { return { m_x, m_y - m_height }; }
+	Vector2 getPoint2()const { return { m_x - m_width / 2, m_y }; }
+	Vector2 getPoint3()const { return { m_x + m_width / 2, m_y }; }
 
 	Player draw()
 	{
-		DrawTriangle(Vector2{ m_x, m_y - m_height}, Vector2{ m_x - m_width / 2, m_y},
-			Vector2{ m_x + m_width / 2, m_y }, BLUE);
+		DrawTriangle(getPoint1(), getPoint2(),
+			getPoint3(), BLUE);
 		return *this;
 	}
+
 
 	Player move()
 	{
@@ -122,13 +151,70 @@ public:
 			StartTimer(&timer, delay);
 			
 		}
+
 		GetElapsed(timer);
 		return *this;
 	}
 
+
+
 };
 
 
+class Enemy
+{
+private:
+	float m_x, m_y;
+	float m_speed;
+	float m_width, m_height;
+	std::vector<Projectile> projectile;
+
+public:
+	Enemy(float x, float y, float speed, float width, float height)
+		: m_x{ x }, m_y{ y }, m_speed{ speed }, m_width{ width }, m_height{ height }
+	{
+
+	}
+
+	~Enemy()
+	{
+
+	}
+
+	Vector2 getPoint1()const { return { m_x - m_width / 2, m_y - m_height };}
+	Vector2 getPoint2()const { return  { m_x, m_y };}
+	Vector2 getPoint3()const { return { m_x + m_width / 2, m_y - m_height }; }
+
+	Enemy draw()
+	{
+		DrawTriangle(getPoint1(), getPoint2(),
+			getPoint3(), RED);
+		return *this;
+	}
+
+	Enemy shoot(float delay, Timer& timer)
+	{
+		for (size_t i = 0; i < projectile.size(); ++i)
+		{
+			projectile.at(i).move();
+			projectile.at(i).draw();
+			if (projectile.at(i).getY() <= 0)
+				projectile.erase(projectile.begin() + i);
+		}
+
+		if (TimerDone(timer))
+		{
+			projectile.push_back({ m_x, m_y - m_height, 5, 150 });
+			StartTimer(&timer, delay);
+
+		}
+
+		GetElapsed(timer);
+		return *this;
+	}
+
+
+};
 
 
 int main()
@@ -137,16 +223,25 @@ int main()
 	InitWindow(GetScreenWidth(), GetScreenHeight(), "Star Conflict");
 
 	Player player{ GetScreenWidth() / 2.0f, GetScreenHeight() * 1.0f - 50, 200, 80, 80 };
+	Enemy enemy{ GetScreenWidth() / 2.0f, 100, 200, 80, 80 } ;
 
-	float shooting_delay = 0.2f;
+	float shooting_delay = 0.4f;
 	Timer shoot_timer{ 0 };
 	while (!WindowShouldClose())
 	{
-		player.move();
-		player.shoot(shooting_delay, shoot_timer);
+		if (!game_over)
+		{
+			player.move();
+			player.shoot(shooting_delay, shoot_timer);
+		}
+		else
+			DrawText("Game Over", GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f, 30, YELLOW);
+		
+
 		BeginDrawing();
 		ClearBackground(BLACK);
 		player.draw();
+		enemy.draw();
 		
 		EndDrawing();
 	}
